63a531a116dfbcbfd1b64fea0fa4c5b9dbcf93fa
yannrichet <richetyann@gmail.com>
2022-02-02 17:57:54 +0100

    choose explicit column for test

--- src/test/java/org/funz/ParserTest.java
+++ src/test/java/org/funz/ParserTest.java
@@ -140,7 +140,7 @@ public class ParserTest {
         Map<String,double[]> m = p.CSV(l, "\\|");
         assert m != null && m.size() > 3 : "Failed to parse with CSV() " + m;
         System.err.println("m.keySet(): "+m.keySet());
-        Object k0 = m.keySet().toArray()[1];
+        Object k0 = "AnnÃ©e";
         double[] vals = m.get(k0);
         assert vals[2] == 1998.0 : "Failed to get 1998 in " + m+" : "+vals[2] ;
     }
